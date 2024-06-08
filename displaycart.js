f10dc5a23da3526d087d7d6aaf5ffe764174f7e5
sengyyy <kiotsuketteneloreto@gmail.com>
2024-06-08 14:00:19 +0800

    last

--- displaycart.js
+++ displaycart.js
@@ -18,8 +18,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 <div class="col-md-2 col-lg-2 col-xl-2">
                     <div class="text-end">${item.quantity}</div> 
                 </div>
-                <div class="col-md-2 col-lg-2 col-xl-2 offset-lg-1">
-                    <h6 class="mb-0 me-3 text-end">₱${(item.price * item.quantity).toFixed(2)}</h6>
+                <div class="col-sm-2 col-md-2 col-lg-2 col-xl-2 offset-lg-1">
+                    <h6 class="mb-0 text-end">₱${(item.price * item.quantity).toFixed(2)}</h6>
                 </div>
                 <div class="col-md-1 col-lg-1 col-xl-1 text-end">
                     <button class="btn btn-remove" onclick="removeItem(${index})"><i class="fas fa-times"></i></button>
